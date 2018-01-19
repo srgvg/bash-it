e5652e983b0c4f318f814e4e4a6488fe4b53e0b8
Serge van Ginderachter <serge@vanginderachter.be>
2018-01-19 16:25:39 +0100

    fix small bug when command ends with backslash

--- custom/set_term_title.bash
+++ custom/set_term_title.bash
@@ -1,1 +1,1 @@
-trap 'echo -ne "\033]2;[$(tty | cut -b 6-) ${PWD}] $(history 1 | sed "s/^[ ]*[0-9]*[ ]*//g")\007"' DEBUG
+trap 'echo -ne "\033]2;[$(tty | cut -b 6-) ${PWD}] $(history 1 | sed "s/^[ ]*[0-9]*[ ]*//g") \007"' DEBUG

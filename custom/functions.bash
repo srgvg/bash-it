fbbb1018df29fa358fe61b8145091cbe7376214b
Serge van Ginderachter <serge@vanginderachter.be>
2018-01-24 09:54:23 +0100

    add function to activate pyenv

--- custom/functions.bash
+++ custom/functions.bash
@@ -45,6 +45,18 @@ function manswitch() {
 }
 
 
+# pyenv
+function pyenv-init() {
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    if command -v pyenv 1>/dev/null 2>&1
+    then
+        eval "$(pyenv init -)"
+    fi
+    eval "$(pyenv virtualenv-init -)"
+}
+
+
 function up() {
   # Quickly CD Out Of n Directories
   local x=''

f4289ed6f496a44c8d9bf623d3128a4f3638886f
Serge van Ginderachter <serge@vanginderachter.be>
2018-02-13 08:16:42 +0100

    add functions from etc/defaults

--- custom/functions.bash
+++ custom/functions.bash
@@ -1,10 +1,10 @@
-
+#
 function apk() {
   # show extended package information
   apt-cache policy $*; apt-cache showpkg $*
 }
 
-
+#
 function aswitch() {
   # switch ansible versions 1-2
   pushd ~/ansible
@@ -25,25 +25,81 @@ function aswitch() {
   popd
 }
 
-
+#
 function bitly() {
   # convert an url to my bitly shorturl
   $HOME/bins/bitly "${1:-}" | tr -d '\n' | tee >(xclip -in -selection clipboard)
 }
 
+#
+function cli_notify() {
+    local urgency
+    local summary
+    local body
 
+	if [ "$#" -eq 0 ]
+	then
+		error "Illegal number of parameters: " \
+			"need '[low|normal|critical]' 'summary' body" || return $?
+		return 1
+	fi
+
+    urgency=${1:-"low"}; shift
+    summary=${1:-" "}; shift
+	body=${*:-" "}
+
+	function error() {
+		echo "$*"
+		return 1
+	}
+
+	case $urgency in
+		"low"|"normal"|"critical")
+			:
+			;;
+		*)
+			error "First parameter should be one of '[low|normal|critical]'" || return $?
+			;;
+	esac
+	echo -e "${summary} (${urgency})\n${body}"
+    notify-send --urgency="${urgency}" --icon=gtk-info \
+		"${summary}" "${body}" || error "libnotify error"
+}
+
+#
 function dl() {
   # quick package search
     dpkg -l | grep `for n in ${*:-^}; do echo -n " -e $n"; done`
 }
 
-
+#
 function manswitch() {
   # This will take you to the relevant part of the man page,
   # so you can see the description of the switch underneath.
   man $1 | less -p "^ +$2";
 }
 
+#
+function i3-launch-job() {
+    if [ -z "$*" ]
+    then
+        echo "Nothing to launch"
+        return
+    fi
+    echo; echo -n "Checking for \"$*\" ... "
+    if pgrep -f "$*"
+    then
+        echo Already running.
+    else
+        launch-screen i3jobs add $*
+    fi
+    echo; echo "----------------------------------------"
+}
+
+#
+function i3-reconnect-i3jobs() {
+    screen -D -r i3jobs
+}
 
 # pyenv
 function pyenv-init() {
@@ -56,7 +112,7 @@ function pyenv-init() {
     eval "$(pyenv virtualenv-init -)"
 }
 
-
+#
 function up() {
   # Quickly CD Out Of n Directories
   local x=''
@@ -66,7 +122,7 @@ function up() {
   cd $x
 }
 
-
+#
 function wcc() {
   # quick character count
   echo "$*" | wc -c

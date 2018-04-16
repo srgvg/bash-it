0e826769ad252b99f3e9610dde751677737703df
Serge van Ginderachter <serge@vanginderachter.be>
2018-04-16 13:10:17 +0200

    set term title only when interactive and login_shell

--- custom/plugins/set_term_title.bash
+++ custom/plugins/set_term_title.bash
@@ -1,1 +1,4 @@
-trap 'echo -ne "\033]2;[$(tty | cut -b 6-) ${PWD}] $(history 1 | sed "s/^[ ]*[0-9]*[ ]*//g") \007"' DEBUG
+if shopt -q login_shell && tty -s
+then
+  trap 'echo -ne "\033]2;[$(tty | cut -b 6-) ${PWD}] $(history 1 | sed "s/^[ ]*[0-9]*[ ]*//g") \007"' DEBUG
+fi
